156520845258ae3f932f144322884f4b9565ff82
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-08-31 14:42:07 -0400

    Updated from Brython Server: 8/31/2017 2:42:06 PM

--- km2miles.py
+++ km2miles.py
@@ -1,1 +1,7 @@
+#Astro Ohnuma
+#8/31/17
+#km2miles.py - converting kilometers to miles
+
+kilometers = int(input('Please enter a number of kilometers'))
+print(kilometers,0.000kilometers,'Kilometers is',kilometers*0.621371,'miles')
 
