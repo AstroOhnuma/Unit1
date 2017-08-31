eec5ea2ecdd3ecfbc6a468b72512dbb17cdb9173
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-08-31 14:29:17 -0400

    Updated from Brython Server: 8/31/2017 2:29:17 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Astro Ohnuma
+#8/31/17
+#face.py - makeing a face
 
+print('   xxxxx   ')
+print('  x     x')
+print('((  o o  ))')
+print('  l  v  l  ')
+print('  l === l  ')
+print('   -----   ')
