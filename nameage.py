bbf7b664a023f77b24634d34449d9783f9728b04
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-01 14:46:18 -0400

    Updated from Brython Server: 9/1/2017 2:46:17 PM

--- nameage.py
+++ nameage.py
@@ -1,3 +1,13 @@
 #Astro Ohnuma
 #8/31/17
 #nameage.py - asking for a users name and age. Then print how many characters are in each name and then how old they will be next year
+
+name = input('Enter your first and last name: ')
+age = int(input('Enter your age: '))
+fname,lname=name.split()
+print('Your first name has',len(fname),'letters')
+print('Your last name has',len(lname),'letters')
+print('Next year you will be',age+1,'years old')
+
+
+
