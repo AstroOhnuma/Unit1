deacd46be8cd05e6bfd215b6fb72fceddea7a65f
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-01 14:29:25 -0400

    Updated from Brython Server: 9/1/2017 2:29:25 PM

--- pythagoreandemo.py
+++ pythagoreandemo.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#9/1/17
+#pythagoreandemo.py - finding the hypotoneuse of a right triangle
 
+from math import sqrt
+
+a = float(input('Enter leg 1: '))
+b = float(input('Enter leg 2: '))
+print('The hypotoneuse is',sqrt(a**2+b**2))
