e0cd84caa3ae3cb1aff786da77dad24aa5e85ef7
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-06 13:59:04 -0400

    Updated from Brython Server: 9/6/2017 1:59:04 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#9/6/17
+#slope.py - calculating the slope of a line given two points
 
+x1 = float(input('Enter the first x position: '))
+y1 = float(input('Enter the first y position: '))
+x2 = float(input('Enter the second x position: '))
+y2 = float(input('Enter the second y position: '))
+print('The slope is',(y2-y1)/(x2-x1))
