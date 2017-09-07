1983494dd9ad2860b2d784de59fad535cb6b6b02
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-07 14:20:55 -0400

    Updated from Brython Server: 9/7/2017 2:20:54 PM

--- warmup2.py
+++ warmup2.py
@@ -1,1 +1,8 @@
+#Astro Ohnuma
+#9/7/17
+#warmup2.py - Entering a name and getting a lucky number
 
+from random import randint
+
+name = input('Enter your name: ')
+print('Your lucky number is',randint(-100,100))
