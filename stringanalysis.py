48ba970c9cbb6998ba9af7e86191660163fdbe5c
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-07 14:54:54 -0400

    Updated from Brython Server: 9/7/2017 2:54:53 PM

--- stringanalysis.py
+++ stringanalysis.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#9/7/17
+#stringanalysis.py - taking a sentence and reports how many words and characters the senteance has. Should also be able to search for a specific character and word and report how many times each occurs
+space = ' '
+sentence = (input('Enter a sentence: '))
+character = (input('Enter a character to search for: '))
+word = (input('Enter a word to search for: '))
+print('Your sentence has',sentence.count(space+1),'words and',len(sentence),'characters')
+print('Your sentence has',sentence.count(word),'of the word',word)
+print('Your sentence has',sentence.count(character),'of the character',character)
 
