7d486845dfc5de0ce583c3b43a7a3c4f556cd072
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-08 14:55:24 -0400

    Updated from Brython Server: 9/8/2017 2:55:23 PM

--- change.py
+++ change.py
@@ -1,1 +1,10 @@
-
+#Astro Ohnuma
+#9/8/17
+#change.py - takes in money and says how many quarters, dimes, nickels, and pennies you will need to make it up
+quarters = 25
+dimes = 10
+nickels = 5
+pennies = 1
+cents = int(input('Enter a number of cents: '))
+print('Quarters:',cents//quarters)
+print('Dimes:',
