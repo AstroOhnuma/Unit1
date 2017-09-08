672797a072ed4db2fc410fb24496647407e5cd49
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-08 14:46:28 -0400

    Updated from Brython Server: 9/8/2017 2:46:28 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,7 @@
+#Astro Ohnuma
+#9/8/17
+#eggs.py - making a program which decides if a number of eggs can fill in a number of cartons
 
+eggs = int(input('Enter a number of eggs: '))
+cartons = int(input('Enter a number of cartons: '))
+print((cartons*12)>=(eggs))
