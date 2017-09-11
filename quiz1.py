d98faa52b0b02a4d2c7f48ec483211e8ef08a356
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-11 14:21:09 -0400

    Updated from Brython Server: 9/11/2017 2:21:08 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#9/11/17
+#quiz1.py - our first quiz!
+
+input('Enter you name: ')
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+print('The sum is',num1+num2)
+print('The product is',num1*num2)
+print('Your lucky number is',(num1*7)+(num2*7))
 
